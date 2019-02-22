5f5ed36c5bd9049c8331f3b20a6ab667c60a2e77
bennamallory <46735177+bennamallory@users.noreply.github.com>
2019-02-21 21:08:36 -0800

    ClockSolver.java working

---
+++ homework03/ClockSolver.java
@@ -0,0 +1,103 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  ClockSolver.java
+ *  Purpose       :  The main program for the ClockSolver class
+ *  @see
+ *  @author       :  Mallory Benna
+ *  Date written  :  2019-02-14
+ *  Description   :  This class provides a bunch of methods which may be useful for the ClockSolver class
+ *                   for Homework 4, part 1.  Includes the following:
+  *
+ *  Notes         :  None right now.  I'll add some as they occur.
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+public class ClockSolver {
+  /**
+   *  Class field definintions go here
+   */
+   private final double MAX_TIME_SLICE_IN_SECONDS  = 1800.00;
+   private final double DEFAULT_TIME_SLICE_SECONDS = 60.0;
+   private final static double EPSILON_VALUE       = 0.1;      // small value for double-precision comparisons
+   static double angleValue = 0.0;
+   static double timeSlice = 0.0;
+
+  /**
+   *  Constructor
+   *  This just calls the superclass constructor, which is "Object"
+   */
+   public ClockSolver() {
+      super();
+   }
+
+
+  /**
+   *  Method to handle all the input arguments from the command line
+   *   this sets up the variables for the simulation
+   */
+   public void handleInitialArguments( String args[] ) {
+     // args[0] specifies the angle for which you are looking
+     //  your simulation will find all the angles in the 12-hour day at which those angles occur
+     // args[1] if present will specify a time slice value; if not present, defaults to 60 seconds
+     // you may want to consider using args[2] for an "angle window"
+
+
+      System.out.println( "\n   Hello world, from the ClockSolver program!!\n\n" ) ;
+      if( 0 == args.length ) {
+         System.out.println( "   Sorry you must enter at least one argument\n" +
+                             "   Usage: java ClockSolver <angle> [timeSlice]\n" +
+                             "   Please try again..........." );
+         System.exit( 1 );
+      }
+
+
+      angleValue = Clock.validateAngleArg(args[0]);
+      if(1 == args.length){
+          timeSlice = Clock.validateTimeSliceArg("60.0");
+      } else {
+          timeSlice = Clock.validateTimeSliceArg(args[1]);
+      }
+
+   }
+
+  /**
+   *  The main program starts here
+   *  remember the constraints from the project description
+   *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
+   *  @param  args  String array of the arguments from the command line
+   *                args[0] is the angle for which we are looking
+   *                args[1] is the time slice; this is optional and defaults to 60 seconds
+   */
+   public static void main( String args[] ) {
+      ClockSolver cs = new ClockSolver();
+      cs.handleInitialArguments( args );
+      Clock clock    = new Clock(timeSlice);
+
+
+      while( true ) {
+
+         clock.tick();
+
+         //If angle they input minus the clock angle is less than epsilon, then print out time
+         //Help by Thomas Kelly  2/21/2019
+         if(clock.getHandAngle() >= angleValue - 1 && clock.getHandAngle() <= angleValue + 1){
+             if(Math.abs(angleValue - clock.getHandAngle()) <= 0.1){
+                 System.out.println("Found target angle at time : " + clock.toString());
+             }
+         }
+
+
+         if(clock.getTotalSeconds() >= 43200){
+             break;
+         }
+      }
+      System.exit( 0 );
+   }
+}
